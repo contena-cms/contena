85b7136ea9ba7bc25fa5e84cf330d73393e75dfd
goxs <lyricsong47@gmail.com>
2026-09-08 10:47:56 +0800

    refactor(administration): align theme entry points

--- src/Administration/Resources/app/administration/src/module/ct-profile/extension/ct-admin-menu/ct-admin-menu.override.spec.ts
+++ src/Administration/Resources/app/administration/src/module/ct-profile/extension/ct-admin-menu/ct-admin-menu.override.spec.ts
@@ -1,20 +1,11 @@
 import { mount } from '@vue/test-utils';
 import profileMenuOverride from './ct-admin-menu.override.vue';
 import { _overridesMap } from 'src/app/adapter/composition-extension-system';
-import type UserConfigService from 'src/core/service/api/user-config.api.service';
-import useTheme from 'src/app/composables/use-theme';
 
 type ProfileOverrideState = {
     acl: {
         can: (privilege: string) => boolean;
     };
-    themeToggle: {
-        value: {
-            icon: string;
-            label: string;
-        };
-    };
-    toggleTheme: () => Promise<void>;
 };
 
 function createWrapper(canUpdateProfile: boolean) {
@@ -49,31 +40,14 @@ function createWrapper(canUpdateProfile: boolean) {
 }
 
 describe('module/ct-profile/extension/ct-admin-menu', () => {
-    beforeAll(() => {
-        // eslint-disable-next-line ct-deprecation-rules/private-feature-declarations
-        Contena.Service().register(
-            'userConfigService',
-            () =>
-                ({
-                    search: jest.fn().mockResolvedValue({ data: {} }),
-                    upsert: jest.fn().mockResolvedValue(undefined),
-                }) as unknown as UserConfigService,
-        );
-    });
-
     beforeEach(() => {
         delete _overridesMap['ct-admin-menu'];
-    });
-
-    afterEach(() => {
-        useTheme().setTheme('system');
     });
 
     it('registers the profile action state for an administrator', () => {
         const { wrapper, state } = createWrapper(true);
 
         expect(state.acl.can('user.update_profile')).toBe(true);
-        expect(state.themeToggle.value.icon).toBe('regular-moon');
 
         wrapper.unmount();
     });
@@ -83,16 +57,6 @@ describe('module/ct-profile/extension/ct-admin-menu', () => {
 
         expect(state.acl.can('user.update_profile')).toBe(false);
 
-        wrapper.unmount();
-    });
-
-    it('toggles the administration theme from the user menu state', async () => {
-        document.documentElement.dataset.theme = 'light';
-        const { wrapper, state } = createWrapper(true);
-
-        await state.toggleTheme();
-
-        expect(document.documentElement.dataset.theme).toBe('dark');
         wrapper.unmount();
     });
 });
--- src/Administration/Resources/app/administration/src/module/ct-profile/extension/ct-admin-menu/ct-admin-menu.override.vue
+++ src/Administration/Resources/app/administration/src/module/ct-profile/extension/ct-admin-menu/ct-admin-menu.override.vue
@@ -6,36 +6,18 @@
             </mt-action-menu-item>
         </router-link>
 
-        <mt-action-menu-item class="ct-profile-menu__theme-toggle" :icon="themeToggle.icon" @click="toggleTheme">
-            {{ $t(themeToggle.label) }}
-        </mt-action-menu-item>
-
         <ct-block-parent />
     </ct-block>
 </template>
 
 <script setup lang="ts">
-import { computed, inject } from 'vue';
+import { inject } from 'vue';
 import type AclService from 'src/app/service/acl.service';
-import useTheme from 'src/app/composables/use-theme';
 
 const acl = inject<AclService>('acl');
 
 if (!acl) {
     throw new Error('The ACL service is required by the profile menu override.');
-}
-
-const { resolvedTheme, saveUserTheme } = useTheme();
-const themeToggle = computed(() => ({
-    icon: resolvedTheme.value === 'dark' ? 'regular-sun' : 'regular-moon',
-    label:
-        resolvedTheme.value === 'dark'
-            ? 'global.ct-admin-menu.themeToggle.switchToLight'
-            : 'global.ct-admin-menu.themeToggle.switchToDark',
-}));
-
-async function toggleTheme(): Promise<void> {
-    await saveUserTheme(resolvedTheme.value === 'dark' ? 'light' : 'dark');
 }
 
 ctDefineOverride({});
